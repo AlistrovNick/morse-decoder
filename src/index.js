bc61e1ad198dd02bd205979e0a0f74a02d30d477
Mikalai Alistrau <alistrovn@gmail.com>
2022-08-04 17:52:52 +0400

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,30 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let words = expr.split('**********');
+    for (let i = 0; i < words.length; i++) {
+        let currentWord = words[i];
+        let letters = [];
+        for (let j = 0; j < currentWord.length; j += 10) {
+            let codeLetter = currentWord.substring(j, j + 10);
+            letters.push(getLetter(codeLetter));
+        }
+        words[i] = letters.join('');
+    }
+    return words.join(' ');
+}
+
+function getLetter(input) {
+    let i = 0;
+    while (input[i] === '0') {
+        i++;
+    }
+    let codeLetter = input.substring(i);
+    let morseCode = '';
+    for (let i = 0; i < codeLetter.length; i += 2) {
+        morseCode += codeLetter[i + 1] === '0' ? '.' : '-';
+    }
+    return MORSE_TABLE[morseCode];
 }
 
 module.exports = {
